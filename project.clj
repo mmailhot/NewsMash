3bb033acb37585269e221defd2bf7258ce4c545f
Marc Mailhot <mmm11105@gmail.com>
2015-01-31 02:17:34 -0500

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,50 @@
+(defproject
+  newsmash
+  "0.1.0-SNAPSHOT"
+  :description
+  "FIXME: write description"
+  :url
+  "http://example.com/FIXME"
+  :dependencies
+  [[prone "0.8.0"]
+   [selmer "0.7.7"]
+   [com.taoensso/tower "3.0.2"]
+   [markdown-clj "0.9.58" :exclusions [com.keminglabs/cljx]]
+   [im.chit/cronj "1.4.3"]
+   [com.taoensso/timbre "3.3.1"]
+   [noir-exception "0.2.3"]
+   [http-kit "2.1.19"]
+   [lib-noir "0.9.5"]
+   [org.clojure/clojure "1.6.0"]
+   [environ "1.0.0"]
+   [ring-server "0.3.1"]]
+  :repl-options
+  {:init-ns newsmash.repl}
+  :jvm-opts
+  ["-server"]
+  :plugins
+  [[lein-ring "0.9.0"] [lein-environ "1.0.0"] [lein-ancient "0.5.5"]]
+  :ring
+  {:handler newsmash.handler/app,
+   :init newsmash.handler/init,
+   :destroy newsmash.handler/destroy,
+   :uberwar-name "newsmash.war"}
+  :profiles
+  {:uberjar {:omit-source true, :env {:production true}, :aot :all},
+   :production
+   {:ring
+    {:open-browser? false, :stacktraces? false, :auto-reload? false}},
+   :dev
+   {:dependencies
+    [[ring-mock "0.1.5"]
+     [ring/ring-devel "1.3.2"]
+     [pjstadig/humane-test-output "0.6.0"]],
+    :injections
+    [(require 'pjstadig.humane-test-output)
+     (pjstadig.humane-test-output/activate!)],
+    :env {:dev true}}}
+  :uberjar-name
+  "newsmash.jar"
+  :main
+  newsmash.core
+  :min-lein-version "2.0.0")
