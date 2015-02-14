cfb338315dff4eb2a4c2a087c9686fb63921b19b
Marc Mailhot <mmm11105@gmail.com>
2015-02-13 21:17:11 -0500

    Adding support for compilation as a Java Servelet for the purposes of getting it running on Bluemix

--- src/newsmash/crawl/utils.clj
+++ src/newsmash/crawl/utils.clj
@@ -16,7 +16,8 @@
 
 (def SERVICE_PW (env :watson-pw))
 
-(def STOP_WORDS (string/split (slurp "./stop-words.txt") #","))
+(def STOP_WORDS
+  ["a" "able" "about" "across" "after" "all" "almost" "also" "am" "among" "an" "and" "any" "are" "as" "at" "be" "because" "been" "but" "by" "can" "cannot" "could" "dear" "did" "do" "does" "either" "else" "ever" "every" "for" "from" "get" "got" "had" "has" "have" "he" "her" "hers" "him" "his" "how" "however" "i" "if" "in" "into" "is" "it" "its" "just" "least" "let" "like" "likely" "may" "me" "might" "most" "must" "my" "neither" "no" "nor" "not" "of" "off" "often" "on" "only" "or" "other" "our" "own" "rather" "said" "say" "says" "she" "should" "since" "so" "some" "than" "that" "the" "their" "them" "then" "there" "these" "they" "this" "tis" "to" "too" "twas" "us" "wants" "was" "we" "were" "what" "when" "where" "which" "while" "who" "whom" "why" "will" "with" "would" "yet" "you" "your"])
 
 (defn is_stop_word? [word]
   (some #(= (string/trim (string/lower-case word)) %) STOP_WORDS))
